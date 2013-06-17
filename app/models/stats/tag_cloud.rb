4570f140f95c98e9f267d2599c4cfb206aa81b80
Rastam Hadi <rastam.hadi@gmail.com>
2013-06-17 23:11:04 +0900

    Extract `tags` and `levels` methods in TagCloud

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -1,25 +1,16 @@
 # tag cloud code inspired by this article
 #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 class TagCloud
-  attr_reader :user, :tags, :min, :divisor, :tags_90days, :min_90days, :divisor_90days
+  attr_reader :user, :min, :divisor, :tags_90days, :min_90days, :divisor_90days
 
   def initialize(user, cut_off = nil)
     @user = user
     @cut_off = cut_off
   end
 
-  # TODO: parameterize limit
   def compute
-    levels=10
-
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-      params += [@cut_off, @cut_off]
-    end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
-
     max, @min = 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @min = [t.count.to_i, @min].min
     }
@@ -27,8 +18,24 @@ class TagCloud
     @divisor = ((max - @min) / levels) + 1
   end
 
+  def tags
+    unless @tags
+      params = [sql(@cut_off), user.id]
+      if @cut_off
+        params += [@cut_off, @cut_off]
+      end
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    end
+    @tags
+  end
+
   private
 
+  def levels
+    10
+  end
+
+  # TODO: parameterize limit
   def sql(cut_off = nil)
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
